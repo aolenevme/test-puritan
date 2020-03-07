a63d313b45b9a49e72b11f9c99594b03cc220398
eshekak <olenev.biz@gmail.com>
2020-03-07 16:31:06 +0300

    feat/add-registrar: Finished clearHandlers

--- src/registrar.js
+++ src/registrar.js
@@ -9,27 +9,32 @@
  **/
 const kinds = new Set(['event', 'fx', 'cofx', 'sub']);
 
+// Create observable for the kind2Id2Handler
+const createKind2Id2HandlerObservable = () =>
+  new Proxy(
+    {},
+    {
+      set: function(target, name, newVal) {
+        console.log(`New value is set: ${name}: ${newVal}`);
+
+        target[name] = newVal;
+      },
+
+      get: function(target, name) {
+        console.log(`Got a value for ${name}`);
+        return target[name];
+      }
+    }
+  );
+
 /**
  * This atom contains a register of all handlers.
  * Contains a two layer map, keyed first by `kind` (of handler), and then `id` of handler.
  * Leaf nodes are handlers.
  * TODO: Write down a correct Observable
  */
-const kind2Id2Handler = new Proxy(
-  {},
-  {
-    set: function(target, name, newVal) {
-      console.log(`New value is set: ${name}: ${newVal}`);
+let kind2Id2Handler = createKind2Id2HandlerObservable();
 
-      target[name] = newVal;
-    },
-
-    get: function(target, name) {
-      console.log(`Got a value for ${name}`);
-      return target[name];
-    }
-  }
-);
 // TODO: You definitely have to elaborate how to get value from getHandler when observable is triggered
 export const getHandler = (kind, id, isRequired) => {
   // If only kind is passed into
@@ -62,4 +67,39 @@ export const registerHandler = (kind, id, handlerFn) => {
   kind2Id2Handler[kind][id] = handlerFn;
 
   return handlerFn;
+};
+
+export const clearHandlers = (kind, id) => {
+  // Clear all kinds
+  if (!kind && !id) {
+    kind2Id2Handler = createKind2Id2HandlerObservable();
+  }
+
+  // Clear all handlers for this kind
+  if (kind && !id) {
+    // No such kind
+    if (kinds.has(kind)) {
+      console.error('No such kind');
+      return;
+    }
+
+    delete kind2Id2Handler[kind];
+  }
+
+  // Clear a single handler for a kind
+  if (kind && id) {
+    // No such kind
+    if (kinds.has(kind)) {
+      console.error('No such kind');
+      return;
+    }
+
+    if (kind2Id2Handler[kind] && kind2Id2Handler[kind][id]) {
+      delete kind2Id2Handler[kind][id];
+    } else {
+      console.warn(
+        `Re-frame: can't clear ${kind} handler for ${id}. Handler not found.`
+      );
+    }
+  }
 };
